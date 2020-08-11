5132a21659880c36373b0b6d9822fbc16d15f37a
jorgeFisde <54189297+jorgeFisde@users.noreply.github.com>
2020-08-11 17:50:55 -0600

    serie fibonacci commit

---
+++ fibonacci.js
@@ -0,0 +1,15 @@
+const limit = 20,array = []
+
+function serieFibonacci(params) {// PARAMS ES EL NUMERO DE LA SERIE QUE DESEA MOSTRAR
+    for (let i = 0; i < limit; i++) {
+        if (i >= 2) {
+            array.push(array[i - 1] + array[i - 2])
+        }else{
+            array.push(i)
+        }
+        
+    }
+    console.log(array[params]);
+}
+
+serieFibonacci(10)
