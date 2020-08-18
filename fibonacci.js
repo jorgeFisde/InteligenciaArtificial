76ade53230d1eeeee436cd4e0e187a9761c83e36
jorgeFisde <54189297+jorgeFisde@users.noreply.github.com>
2020-08-18 12:41:05 -0600

    recursividad

--- fibonacci.js
+++ fibonacci.js
@@ -16,4 +16,23 @@ function serieFibonacci(params) {// PARAMS ES EL NUMERO DE LA SERIE QUE DESEA MO
     console.log(array[params]);
 }
 
-serieFibonacci(10)
+
+
+
+// RECURSIVO
+
+function fiboncciRecursivo(number) {
+
+    if (number == 1) {
+        return 0
+    }
+    if (number == 2 ) {
+        return 1
+    }
+
+    return  fiboncciRecursivo(number - 1) + fiboncciRecursivo(number - 2)
+    
+}
+
+const numb = 5
+console.log(`El fibonacci numero ${numb} es:` ,fiboncciRecursivo(numb));
