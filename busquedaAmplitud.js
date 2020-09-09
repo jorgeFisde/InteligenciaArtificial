056a065354fc1b9fc692fefaaa9cf99859d58e00
jorgeFisde <54189297+jorgeFisde@users.noreply.github.com>
2020-09-09 11:03:14 -0600

    adfasd

--- busquedaAmplitud.js
+++ busquedaAmplitud.js
@@ -8,3 +8,8 @@ function cambio() {
 }
 
 console.log(cambio());
+
+var machin = cad_inicial.split(''); // separar el string en array
+var hola = machin.shift(); // shift saca el primer elemento y lo asignamos a hola
+machin.push(hola); // lo agregamos a la ultima pocicion del arreglo
+console.log(machin);
